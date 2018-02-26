febc7382e3a374230f7ac47103be2af41cd12457
Anurag Singh <anuragsingh.dakshana15@gmail.com>
2018-02-27 01:53:17 +0530

    Update max_files.py

--- max_files.py
+++ max_files.py
@@ -8,7 +8,7 @@ def get_size():
     total_size = 0
     maxm=0
     h=[]
-    q=0
+    #q=0
 
     for drive in win32api.GetLogicalDriveStrings().split('\000')[:-1]:
         dirpath = drive
@@ -16,13 +16,13 @@ def get_size():
         for dirpath, dirnames, filenames in os.walk(dirpath):
             for f in filenames:
                 fp = os.path.join(dirpath, f)
-                q += 1
+                #q += 1
 
                 
                 try:
                     b= os.path.getsize(fp)
                     total_size += b
-                    print(q)
+                    #print(q)
                     if len(h) < 10:
                             heapq.heappush(h, (float(b)/1000000,f))
                     else:
